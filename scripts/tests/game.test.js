4fa9494d3f77a0019ff5b790115bbc50b69db5b5
Daniel Harris <dwh261@gmail.com>
2024-08-14 19:30:56 +0000

    Built out and tested first function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,9 +2,8 @@
  * @jest-environment jsdom
  */
 
-const { exportAllDeclaration } = require("@babel/types");
 const { default: test } = require("node:test");
-const { describe } = require("yargs");
+const { game } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -12,10 +11,22 @@ beforeAll(() => {
     document.open();
     document.write(fileContents);
     document.close();
-})
+});
 
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
-        exportAllDeclaration("score" in game).toBe(true);
+        expect("score" in game).toBe(true);
     });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contains correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    })
 });
